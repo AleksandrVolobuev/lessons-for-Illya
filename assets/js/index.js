2aa6a29629681e37c19dba4764191fa3f2ca6340
Alek Volobuev <aleksandrvolobuev7676@gmail.com>
2023-08-03 15:28:47 +0300

    Initiale commit

---
+++ assets/js/index.js
@@ -0,0 +1,66 @@
+//let a = 1;
+//const cat = "Mursik";
+
+//let number = a + b;
+
+//console.log(number);
+
+//a = 5;
+//b = 6;
+
+//number = a + b;
+
+//console.log(number);
+
+//a = 7;
+//b = 10;
+
+//number = a + b;
+
+//console.log(number);
+
+//function sumNumber(a, b, c, m) {
+//  return a + b + c + m;
+//}
+
+//console.log(sumNumber(prompt(),prompt(),prompt(),prompt()));
+
+//Number , String, undefined, null,
+//Boolean ,BigInt, Symbol, Object
+
+//let value = true;
+//console.log(typeof value);
+//console.log(value)
+
+//value = String(value)
+//console.log(typeof value);
+//console.log(value);
+
+//let str = "3" + 7;
+//console.log(typeof str);
+//console.log(str);
+////str = Number(str);
+//console.log(+str);
+//console.log(typeof str);
+//console.log(str);
+
+//let x = 6;
+//x++
+
+//alert(+x);
+
+//let counter = 1;
+//let a = ++counter;
+
+//alert(a);
+
+//let a = (1+2,3+4);
+
+//alert(a);
+
+
+
+
+let a = null +1;
+
+alert(a);
